5b18be529cd5be66450d92dec8b90b975ce69888
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2024-01-01 20:07:33 +0200

    Week2-Labs-Assignments

--- C-Language/C-for-Everyone/C2-Structured-Programming/Week2-Labs-Assignments/Struct_Dates.c
+++ C-Language/C-for-Everyone/C2-Structured-Programming/Week2-Labs-Assignments/Struct_Dates.c
@@ -1,11 +1,3 @@
-/*
-	Author: Abdelruhman Kamal
-	Program: Write enumerated types that supports dates such as december 12. 
-	Then add a function that produces a next day. 
-	So nextday(date) of december 12 is december 13. 
-	Also write a function printdate(date) that prints a date legiblyThe function can assume that February has 28 days and it most know 	how many days are in each month. 
-	Use a struct with two members; one is the month and the second is the depot the month—an int (or short). 
-*/
 /******************************************************************************
  * Copyright (C) 2024 by Abdelrahman Kamal - C-For-Everyone
  *****************************************************************************/
